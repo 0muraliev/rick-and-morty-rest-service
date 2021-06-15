62ffb5dc1040de24f62e860b72d6577fc59a4758
0muraliev <omuraliev2019@gmail.com>
2021-06-15 09:33:41 +0600

    Add url argument

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
-def main():
-    return jsonify(message='hello world!')
+@app.route('/api/test_video')
+def test():
+    return jsonify(
+        url='https://docs.google.com/file/d/0B4BsAbG4atWHQzVfLUU3UnhhZTA/edit?resourcekey=0-LSbA63dXOK1G-76LJT4SLw'
+    )
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
