7590b35b48a4c2a5f0312d6cd290ae267c837c2d
0muraliev <omuraliev2019@gmail.com>
2021-06-27 00:25:45 +0600

    Add seasons API View

--- main.py
+++ main.py
@@ -3,10 +3,31 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/api/test_video')
-def test():
+@app.route('/api/seasons/')
+def seasons():
     return jsonify(
-        url='https://docs.google.com/file/d/0B4BsAbG4atWHQzVfLUU3UnhhZTA/edit?resourcekey=0-LSbA63dXOK1G-76LJT4SLw'
+        seasons={
+            1: {
+                'id': 1,
+                'urlImage': 'https://static.wikia.nocookie.net/rickandmorty/images/7/70/Rick_and_Morty_Season_1.jpg/revision/latest?cb=20161125224650',
+                'title': 'Season 1'
+            },
+            2: {
+                'id': 2,
+                'urlImage': 'https://static.wikia.nocookie.net/rickandmorty/images/d/d5/Season2.jpg/revision/latest?cb=20200718092925',
+                'title': 'Season 2'
+            },
+            3: {
+                'id': 3,
+                'urlImage': 'https://static.wikia.nocookie.net/rickandmorty/images/b/be/Season3.jpg/revision/latest?cb=20200718093040',
+                'title': 'Season 3'
+            },
+            4: {
+                'id': 4,
+                'urlImage': 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/3503/35033712_sa.jpg',
+                'title': 'Season 4'
+            }
+        }
     )
 
 
